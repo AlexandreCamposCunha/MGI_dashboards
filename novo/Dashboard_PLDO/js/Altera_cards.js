eb30a5f1900f6e7333a5c6afb8c0ed247261fdf5
AlexandreCamposCunha <accunha@cetem.gov.br>
2023-04-25 14:10:24 -0300

    Filtros dos controles criado

--- novo/Dashboard_PLDO/js/Altera_cards.js
+++ novo/Dashboard_PLDO/js/Altera_cards.js
@@ -7,25 +7,67 @@ var divOriginal_title1 = document.getElementById("title_card1").innerHTML;
 var divOriginal_title2 = document.getElementById("title_card2").innerHTML;
 var divOriginal_title3 = document.getElementById("title_card3").innerHTML;
 var divOriginal_title4 = document.getElementById("title_card4").innerHTML;
-function altera_cards(dados, ano_selecionado){
-        
+function altera_cards(dados, dados_selecionado){
+
+    // Cria o json filtrado para anos
+    let novoObjeto={};
+    if(dados_selecionado[0].length > 0){
       //Alterando array de strings para anos
-      const arrayDeInteiros = ano_selecionado[0].map(str => parseInt(str, 10))
+      const arrayDeInteiros = dados_selecionado[0].map(str => parseInt(str, 10))
       console.log(arrayDeInteiros);
       //Indica a variavel de anos escolhidos
       const anosDesejados = arrayDeInteiros;
       //Filtra o json com os anos selecionados
       const chavesFiltradas = Object.keys(dados).filter(chave => anosDesejados.includes(dados[chave].Ano));
       //Cria um json filtrado com os dados escolhidos
-      const novoObjeto = Object.fromEntries(
+      novoObjeto = Object.fromEntries(
         chavesFiltradas.map(chave => [chave, dados[chave]])
       );
+      }
+    
+    else{
+        novoObjeto = dados;
+    }
 
-      console.log(novoObjeto);
+    //Cria o filtro para o grupo
+    if(dados_selecionado[1].length > 0){
+        //Indica a variavel de anos escolhidos
+        const Grupos_Desejados = dados_selecionado[1];
+        //Filtra o json com os anos selecionados
+        const chavesFiltradas = Object.keys(novoObjeto).filter(chave => Grupos_Desejados.includes(novoObjeto[chave].Grupo));
+        //Cria um json filtrado com os dados escolhidos
+        novoObjeto = Object.fromEntries(
+          chavesFiltradas.map(chave => [chave, novoObjeto[chave]])
+        );
+        }
+      
+      else{
+          novoObjeto = novoObjeto;
+      }
+
+    
+      //Cria o filtro para os poderes
+    if(dados_selecionado[2].length > 0){
+        //Indica a variavel de anos escolhidos
+        const Grupos_Desejados = dados_selecionado[2];
+        //Filtra o json com os anos selecionados
+        const chavesFiltradas = Object.keys(novoObjeto).filter(chave => Grupos_Desejados.includes(novoObjeto[chave].Poder));
+        //Cria um json filtrado com os dados escolhidos
+        novoObjeto = Object.fromEntries(
+          chavesFiltradas.map(chave => [chave, novoObjeto[chave]])
+        );
+        }
+      
+      else{
+          novoObjeto = novoObjeto;
+      }
+
+
+   console.log(novoObjeto);
 
       
     
-        if(anosDesejados.length === 0){
+        if(dados_selecionado.length === 0){
             
             document.getElementById("info_card1").innerHTML=divOriginal_valor1
             document.getElementById("info_card2").innerHTML=divOriginal_valor2
